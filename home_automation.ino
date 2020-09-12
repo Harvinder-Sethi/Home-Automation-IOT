76c1d524544dcb98778a3f106001d0e5ccbf0538
Harvinder Sethi <harshsethi1996@yahoo.com>
2020-09-12 16:30:04 +1000

    Add files via upload

---
+++ home_automation.ino
@@ -0,0 +1,39 @@
+int state;
+int LED=0;
+int x=0; // Initialize LED status variable 
+void setup()
+{ Serial.begin(9600); // Configure dev machine serial port // baud rate
+//Serial1.begin(9600); // Configure baud rate for Bluetooth // connection
+pinMode(2, OUTPUT); 
+pinMode(3, OUTPUT); }
+void loop() 
+{ if(Serial.available() > 0)
+
+  { 
+    state = Serial.read();
+  }
+ if (state == '1')
+ { 
+  digitalWrite(2,HIGH); 
+ } 
+   else if(state == '0')
+  {
+    digitalWrite(2,LOW);
+  }
+  else if(state == '2')
+  {
+    digitalWrite(3,HIGH);
+  }
+  else if(state == '3')
+  {
+    digitalWrite(3,LOW);
+  } 
+  else if(state == '8')
+  {
+    digitalWrite(9,HIGH);
+  } 
+  else if(state == '9')
+  {
+    digitalWrite(9,LOW);
+  } 
+}
